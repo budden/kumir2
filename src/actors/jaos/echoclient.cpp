30419eb3f621fb15197631f6376d79c8f27f5de2
Denis Budyak <budden73@gmail.com>
2021-07-31 15:07:06 +0300

    Убрал замыкания и более хорошо разложил по объектам

--- src/actors/jaos/echoclient.cpp
+++ src/actors/jaos/echoclient.cpp
@@ -20,45 +20,63 @@ ContainerThread::ContainerThread(bool debug, QObject *parent, QCoreApplication *
     qDebug() << "entered ContainerThread::ContainerThread";
 }
 
+QJakClientEventLoop::QJakClientEventLoop(QObject *parent) : QEventLoop(parent) {
+    connect(&socket, &QTcpSocket::readyRead, this,   
+        &QJakClientEventLoop::onSocketReadyRead);
+
+    connect(&socket, &QAbstractSocket::connected, this, 
+        &QJakClientEventLoop::onSocketConnected) ;
+
+    connect(&socket, &QTcpSocket::disconnected, this, 
+        &QJakClientEventLoop::onSocketDisconnected);
+
+};
+
+
+void QJakClientEventLoop::onSocketReadyRead() {
+    {
+        char in_data[1024];
+        qDebug() << "entered readyToRead";
+        if (socket.canReadLine()) {
+            socket.readLine(in_data,sizeof(in_data));
+        qDebug() << "onReadRead: readLine() got " << in_data; 
+        socket.close();
+        }
+    }
+}
+
+void QJakClientEventLoop::onSocketConnected() {
+        qint64 bytesWrittenNow = socket.write(QByteArray("1:17"));
+        // Q_ASSERT(tcpClient.flush()); // пусть хоть упадёт, не знаю, как быть пока что.
+        qDebug() << "Leaving startTransfer, bytes written to the buffer = " << bytesWrittenNow;        
+    }
+
+
+void QJakClientEventLoop::onSocketDisconnected() {
+        qDebug() << "QTcpSocket::disconnected signaled";
+        quit();    
+}
+
+void ContainerThread::startConnecting(int port) {
+    eventLoop->socket.connectToHost(QHostAddress::LocalHost, port);
+}
+
+void ContainerThread::startDisconnecting() {
+    eventLoop->socket.disconnect();
+}
+
+
+
 // https://stackoverflow.com/a/60192923
 void ContainerThread::run()  {
     qDebug() << "entered ContainerThread::run";
     // container->startWork();
-    QEventLoop eventLoop;
-    QTcpSocket *socket = new QTcpSocket(&eventLoop);
-    connect(socket, &QTcpSocket::readyRead, &eventLoop, [socket]
-    {
-        char in_data[1024];
-        qDebug() << "entered readyToRead";
-        if (socket->canReadLine()) {
-            socket->readLine(in_data,sizeof(in_data));
-        qDebug() << "onReadRead: readLine() got " << in_data; 
-        socket->close();
-        }
-    });
+    eventLoop = new QJakClientEventLoop(this) ;
 
-    socket->connect(socket, &QAbstractSocket::connected, &eventLoop, [socket]{
-        qint64 bytesWrittenNow = socket->write(QByteArray("1:17"));
-        // Q_ASSERT(tcpClient.flush()); // пусть хоть упадёт, не знаю, как быть пока что.
-        qDebug() << "Leaving startTransfer, bytes written to the buffer = " << bytesWrittenNow;        
-    });
+    startConnecting(8967);
+    eventLoop->exec();
 
-    // Quit the loop (and thread) if the socket it disconnected. You could also try
-    // reconnecting
-    socket->connect(socket, &QTcpSocket::disconnected, &eventLoop, [&eventLoop]
-    {
-        qDebug() << "QTcpSocket::disconnected signaled";
-        eventLoop.quit();
-    });
-
-    socket->connectToHost(QHostAddress::LocalHost, 8967);
-
-    eventLoop.exec();
-
-    delete socket;
-
-    qDebug() << "Thread object becomes an orphan, delete it please!";
-    // app->quit();
+    app->quit();
 
 }
 
--- src/actors/jaos/echoclient.h
+++ src/actors/jaos/echoclient.h
@@ -9,19 +9,43 @@
 #include <QTcpSocket>
 #include <QtCore/QCoreApplication>
 
+
+class QJakClientEventLoop : public QEventLoop {
+
+Q_OBJECT
+public:
+    QTcpSocket socket;
+    explicit QJakClientEventLoop(QObject *parent = nullptr);
+
+public slots:
+    void onSocketReadyRead();
+    void onSocketConnected();
+    void onSocketDisconnected();
+
+};
+
 class ContainerThread : public QThread {
 
 Q_OBJECT
 
 public: 
 
-QCoreApplication *app;
 explicit ContainerThread(bool debug, QObject *parent, QCoreApplication *inApp);
+
+
+QCoreApplication *app;
+QJakClientEventLoop *eventLoop;
+
 
 void run() override;
 
 Q_SIGNALS:
     void onEventLoopExitingg();
+
+public slots:
+    void startConnecting(int port);
+    void startDisconnecting();
+
 
 };
 
